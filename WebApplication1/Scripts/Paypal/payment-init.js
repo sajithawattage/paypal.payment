b6ee4382e3608d59037ced202478843fddbb28c7
Sajitha Wattage <sajitha.wattage@gmail.com>
2018-01-22 01:53:05 +0800

    First Commit

---
+++ WebApplication1/Scripts/Paypal/payment-init.js
@@ -0,0 +1,34 @@
+﻿paypal.Button.render({
+    env: 'sandbox', // 'production' Or 'sandbox',
+
+    commit: true, // Show a 'Pay Now' button
+
+    style: {
+        color: 'gold',
+        size: 'small'
+    },
+
+    payment: function (data, actions) {
+        /* 
+         * Set up the payment here 
+         */
+    },
+
+    onAuthorize: function (data, actions) {
+        /* 
+         * Execute the payment here 
+         */
+    },
+
+    onCancel: function (data, actions) {
+        /* 
+         * Buyer cancelled the payment 
+         */
+    },
+
+    onError: function (err) {
+        /* 
+         * An error occurred during the transaction 
+         */
+    }
+}, '#paypal-button');
